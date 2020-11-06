1cdf5c8449f2f7795121f37cc6cee39bea103757
Johanna <2myadverts@gmail.com>
2020-11-06 08:26:54 +0100

    mongodb uri

--- config/db.config.js
+++ config/db.config.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/latin-dance-db';
 
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true })
+mongoose.connect(MONGODB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true //added after failur message
+    })
     .then(() => console.info(`Successfully connected to the database ${MONGODB_URI}`))
     .catch(error => console.error(`An error ocurred trying to connect to de database ${MONGODB_URI}`, error));
 
